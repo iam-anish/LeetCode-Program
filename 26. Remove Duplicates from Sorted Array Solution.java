05c6e242e8ae215dd3c7e6f561fc31ff0519c4c0
Anish Mathakiya <86679880+iam-anish@users.noreply.github.com>
2023-01-03 22:53:22 +0530

    Create 26. Remove Duplicates from Sorted Array Solution.java

---
+++ 26. Remove Duplicates from Sorted Array Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        if(nums.length == 0)
+            return 0;
+        
+        int addIndex = 1; //index that unique characters will be inserted at
+
+        for(int i = 0; i < nums.length - 1; i++) {
+            if(nums[i] < nums[i + 1]){ //if true, num[i + 1] is a new uniquenumber
+              nums[addIndex] = nums[i + 1];
+              addIndex++;
+            }
+        }
+        return addIndex;
+    }
+}
