3168687b842ccf48485adb521299f377c46d471f
Anish Mathakiya <86679880+iam-anish@users.noreply.github.com>
2023-01-20 22:43:54 +0530

    Create 122. Best Time to Buy and Sell Stock II Solution.java

---
+++ 122. Best Time to Buy and Sell Stock II Solution.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int ans = 0;
+        for(int i=1; i<prices.length; i++) {
+            int dif = prices[i] - prices[i-1];
+            if(dif > 0) ans += dif;
+        }
+        return ans;
+    }
+}
