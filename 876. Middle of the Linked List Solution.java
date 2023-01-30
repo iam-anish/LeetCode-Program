314150b71c7d5086bb7dfa9eba92be96989247c6
Anish Mathakiya <86679880+iam-anish@users.noreply.github.com>
2023-01-30 23:04:21 +0530

    Create 876. Middle of the Linked List Solution.java

---
+++ 876. Middle of the Linked List Solution.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode temp = head;
+        int size = 0;
+        while(temp!=null){
+             size++;
+             temp = temp.next;
+        }
+        temp = head;
+        int i = 0;
+        while(i<size/2){
+            temp = temp.next;
+            i++;
+        }
+        return temp;
+    }
+}
