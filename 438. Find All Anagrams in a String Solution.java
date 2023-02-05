3e6de2df63bdf32130081a8edc919bcdc8b1254d
Anish Mathakiya <86679880+iam-anish@users.noreply.github.com>
2023-02-05 23:39:35 +0530

    Create 438. Find All Anagrams in a String Solution.java

---
+++ 438. Find All Anagrams in a String Solution.java
@@ -0,0 +1,42 @@
+class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
+        List<Integer> res = new ArrayList<>();
+
+        if(s.length() < p.length()){
+            return res;
+        }
+        int[] ct1 = new int[26];
+        int[] ct2 = new int[26];
+
+        int k = p.length();
+
+        for(char it : p.toCharArray()){
+            ct1[it-'a']++;
+        }
+
+        int i = 0;
+
+        while(i < k){
+            ct2[s.charAt(i++)-'a']++;
+        }
+
+        k = s.length();
+        int j = 0;
+
+        if(Arrays.equals(ct1, ct2)){
+            res.add(j);
+        }
+
+        while(i < k){
+
+            ct2[s.charAt(j++)-'a']--;
+            ct2[s.charAt(i++)-'a']++;
+
+            if(Arrays.equals(ct1, ct2)){
+                res.add(j);
+            }
+        }
+
+        return res;
+    }
+}
